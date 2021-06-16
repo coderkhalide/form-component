92fd63a4f5d87b5739377e04f713e76887049357
Khalid Saifullah <khalideofficial@gmail.com>
2021-06-16 23:21:43 +0600

    Added animation

--- src/components/DeviceForm/index.js
+++ src/components/DeviceForm/index.js
@@ -3,6 +3,7 @@ import FormInput from '../FormInput'
 import { useState } from 'react'
 import FormDropdown from '../FormDropdown'
 import FormButton from '../FormButton'
+import Fade from 'react-reveal/Fade';
 
 const conditions = [
     {
@@ -47,68 +48,82 @@ function DeviceForm({ options, title, onSubmit }) {
     
     return (
         <div className="deviceForm">
-            <h1 className="deviceForm__title">{title}</h1>
+            <Fade top>
+                <h1 className="deviceForm__title">{title}</h1>
+            </Fade>
             <div className="deviceForm__content">
-               <div className="deviceForm__form">
-                    <form onSubmit={handleSubmit} >
-                        <FormDropdown 
-                            options={options} 
-                            onChange={(val) => setDevice(val)}
-                            placeholder="Select device"
-                        />
-                        {device && (
-                            <>
-                                <FormDropdown 
-                                    options={conditions} 
-                                    onChange={(val) => setCondition(val)}
-                                    placeholder="Condition"
-                                />
-                                <FormInput 
-                                    placeholder="Quantity (min 10)" 
-                                    value={quantity}
-                                    onChange={setQuantity}
-                                    type="number"
-                                    min="10"
-                                />
-                                <FormInput 
-                                    placeholder="First name" 
-                                    value={fName}
-                                    onChange={setFName}
-                                />
-                                <FormInput 
-                                    placeholder="Last name" 
-                                    value={lName}
-                                    onChange={setLName}
-                                />
-                                <FormInput 
-                                    placeholder="Your email" 
-                                    value={email}
-                                    onChange={setEmail}
-                                />
-                                <FormInput 
-                                    placeholder="Your phone" 
-                                    value={phone}
-                                    onChange={setPhone}
-                                />
-                                <FormInput 
-                                    placeholder="Your address" 
-                                    value={address}
-                                    onChange={setAddress}
-                                />
-                                
-                                <FormButton 
-                                    title="Submit" 
-                                    disable={(device && condition && quantity && fName && lName && address && phone && email) ? false : true}
-                                />
-                            </>
-                        )}
-                    </form>
-                </div>
+                <Fade bottom>
+                    <div className="deviceForm__form">
+                        <form onSubmit={handleSubmit} >
+                            <FormDropdown 
+                                options={options} 
+                                onChange={(val) => setDevice(val)}
+                                placeholder="Select device"
+                            />
+                            {device && (
+                                <>
+                                    <FormDropdown 
+                                        options={conditions} 
+                                        onChange={(val) => setCondition(val)}
+                                        placeholder="Condition"
+                                    />
+                                    <div className="deviceForm__inputs">
+                                        <Fade bottom cascade>
+                                            <FormInput 
+                                                placeholder="Quantity (min 10)" 
+                                                value={quantity}
+                                                onChange={setQuantity}
+                                                type="number"
+                                                min="10"
+                                            />
+                                            <div className="deviceForm__halfGrid">
+                                                <FormInput 
+                                                    placeholder="First name" 
+                                                    value={fName}
+                                                    onChange={setFName}
+                                                />
+                                                <FormInput 
+                                                    placeholder="Last name" 
+                                                    value={lName}
+                                                    onChange={setLName}
+                                                />
+                                            </div>
+                                            <FormInput 
+                                                placeholder="Your email" 
+                                                value={email}
+                                                onChange={setEmail}
+                                            />
+                                            <FormInput 
+                                                placeholder="Your phone" 
+                                                value={phone}
+                                                onChange={setPhone}
+                                            />
+                                            <FormInput 
+                                                placeholder="Your address" 
+                                                value={address}
+                                                onChange={setAddress}
+                                            />
+                                            
+                                            <FormButton 
+                                                title="Submit" 
+                                                disable={(device && condition && quantity && fName && lName && address && phone && email) ? false : true}
+                                            />
+                                        </Fade>
+                                    </div>
+                                </>
+                            )}
+                        </form>
+                    </div>
+                </Fade>
                 {device && (
                     <div className="deviceForm__image">
-                        <img src={device.image} alt="" />
+                        <Fade bottom>
+                            <img src={device.image} alt="" />
+                        </Fade>
                         {(condition) && (
-                            <h2 className="deviceForm__price">Price ${device.price[condition.value]}</h2>
+                            <Fade bottom>
+                                <h2 className="deviceForm__price">Price ${device.price[condition.value]}</h2>
+                            </Fade>
                         )}
                     </div>
                 )}
